063b19ff98362dc496ce17881db014b42090f6cc
thesaucemachine <skyler.saucedo@gmail.com>
2024-05-14 15:09:34 -0500

    added gitignore

--- pipelines/pull_data_from_labelstudio.py
+++ pipelines/pull_data_from_labelstudio.py
@@ -5,16 +5,21 @@ we need to convert to images and save to S3
 """
 
 from label_studio_sdk import Client
+import dotenv
+import os
 
-API_ROOT = "http://18.218.125.193:8080"
+dotenv.load_dotenv()
+
+
+
+API_ROOT = os.getenv("LS_API_TOKEN")
 
 id = "1"
 exportType = "JSON"
-api_token = "4498d6dbce82095f126fc6b6b594f21342e1351c"
 output_filename = 'may14annotations.json'
 
 # Initialize the Label Studio SDK client with the additional headers
-ls = Client(url=API_ROOT, api_key=api_token)
+ls = Client(url=API_ROOT, api_key=API_ROOT)
 
 # Get the project
 project = ls.get_project(id)
