8f500c0ed35234c07a1679636fefe658b43c23fd
thesaucemachine <skyler.saucedo@gmail.com>
2024-05-31 07:37:01 -0500

    bypassing .rec approach, added labels json script

--- utils/current_labels.py
+++ utils/current_labels.py
@@ -1,8 +1,9 @@
 ### use this to store current labels
+import numpy as np
 
 def one_hot_json():
-    """convert labels into JSON packet"""
-    
+    """make list of items of interest"""
+
     categories = [
         {'class_id' : 0, 'name' : 'blue_tape'},
         {'class_id' : 1, 'name' : 'black_tape'},
@@ -12,20 +13,14 @@ def one_hot_json():
     
     return categories
 
+def search(name, x):
+    
+    if len([e for e in x if e['name'] == name]) > 0:
+        return [e for e in x if e['name'] == name][0]['class_id']  
+    else:
+        return np.NaN 
 
 def one_hot_label(label):
-    """
-    soon to be depreciated...
-    one hot encode label from string to int
-    """
-    r = 4
-    if label == 'blue_tape':
-        r = 0
-    if label == 'black_tape':
-        r = 1
-    if label == 'gum':
-        r = 2
-    if label == 'leaf':
-        r = 3
-        
-    return r
+    """return class_id from label name"""
+    cats = one_hot_json()
+    return search(label,cats)
